f68b4b9b133d3c8ecb9826af9736c8c1fca64e49
Laurens Van Houtven <_@lvh.cc>
2012-10-08 12:43:14 +0200

    Add caveat about UsernamePassword already being an IUsernamePassword implementation

--- maxims/credentials.py
+++ maxims/credentials.py
@@ -5,6 +5,10 @@ from twisted.cred import credentials
 class UsernamePassword(item.Item):
     """
     A stored username and password.
+
+    Note that although this class is an ``IUsernamePassword`` implementation,
+    you should still use the ``instantiate`` method to get independent
+    ``IUsernamePassword`` providers.
     """
     username = attributes.bytes(allowNone=False)
     password = attributes.bytes(allowNone=False)
