258901bcb5ceb9acb08a23f419de19583ce706b1
Laurens Van Houtven <_@lvh.cc>
2012-11-05 16:04:15 +0100

    Extranew version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='maxims',
-      version='20121105',
+      version='20121106',
       description='A set of tools and reusbale items for Axiom',
       url='https://github.com/lvh/maxims',
 
