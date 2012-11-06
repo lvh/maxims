21dce63c8a8bbeb4d6f97f8f8eff855cde183730
Laurens Van Houtven <_@lvh.cc>
2012-11-06 18:50:52 +0100

    Credentials work Credentials now implement their interface directly (no more annoying instantiate).

--- maxims/credentials.py
+++ maxims/credentials.py
@@ -1,17 +1,15 @@
+"""
+Stored credentials.
+"""
 from axiom import attributes, item
 from twisted.cred import credentials
+from zope import interface
 
 
+@interface.implementer(credentials.IUsernamePassword)
 class UsernamePassword(item.Item):
     """
     A stored username and password.
-
-    Note that although this class is an ``IUsernamePassword`` implementation,
-    you should still use the ``instantiate`` method to get independent
-    ``IUsernamePassword`` providers.
     """
     username = attributes.bytes(allowNone=False)
     password = attributes.bytes(allowNone=False)
-
-    def instantiate(self):
-        return credentials.UsernamePassword(self.username, self.password)
--- maxims/test/test_credentials.py
+++ maxims/test/test_credentials.py
@@ -1,3 +1,6 @@
+"""
+Tests for stored credentials.
+"""
 from twisted.cred import credentials
 from twisted.trial import unittest
 
@@ -5,12 +8,22 @@ from maxims import credentials as mc
 
 
 class UsernamePasswordTests(unittest.TestCase):
-    def test_instantiate(self):
+    """
+    Tests for a storable username and password pair.
+    """
+    def test_interface(self):
+        """
+        Tests that the item implements the correct interface.
+        """
+        I, C = credentials.IUsernamePassword, mc.UsernamePassword
+        self.assertTrue(I.implementedBy(C))
+
+
+    def test_simple(self):
+        """
+        Tests that attributes can be accessed as expected.
+        """
         username, password = "username", "password"
         up = mc.UsernamePassword(username=username, password=password)
-        c = up.instantiate()
-
-        self.assertEqual(c.username, username)
-        self.assertEqual(c.password, password)
-
-        self.assertTrue(credentials.IUsernamePassword.providedBy(c))
+        self.assertEqual(up.username, username)
+        self.assertEqual(up.password, password)
