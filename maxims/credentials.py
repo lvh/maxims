271fcf3fe374b709fefbb4aacc1739033115b5c6
Laurens Van Houtven <_@lvh.cc>
2012-11-14 16:44:47 +0100

    Add warning about plaintext credentials

--- maxims/credentials.py
+++ maxims/credentials.py
@@ -10,6 +10,9 @@ from zope import interface
 class UsernamePassword(item.Item):
     """
     A stored username and password.
+
+    Since the password is stored in plaintext, this is primarily useful for
+    credentials you use, not for credentials you check against.
     """
     username = attributes.bytes(allowNone=False)
     password = attributes.bytes(allowNone=False)
